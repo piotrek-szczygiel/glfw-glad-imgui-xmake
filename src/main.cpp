186c1324407cc4937038b010d34bf0bc4e6088ab
Piotr Szczygieł <szczygiel.p@gmail.com>
2021-06-27 17:33:46 +0200

    Add working template

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,159 @@
-#include <iostream>
-
-#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
+#include <fmt/core.h>
+#include <glad/glad.h>
 #include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
 
-const GLuint WIDTH = 800, HEIGHT = 600;
+#define SPDLOG_FMT_EXTERNAL
+#include <spdlog/spdlog.h>
 
-int main()
-{
-    std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
-    // Init GLFW
-    glfwInit();
-    // Set all the required options for GLFW
+int main(int, char**) {
+    glfwSetErrorCallback([](int error, const char* description) { spdlog::error("GLFW error {}: {}", error, description); });
+    if (!glfwInit()) return 1;
+
+#if defined(__APPLE__)
+    // GL 3.2 + GLSL 150
+    const char* glsl_version = "#version 150";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+#else
+    // GL 3.0 + GLSL 130
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
 
-    // Create a GLFWwindow object that we can use for GLFW's functions
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);
+    auto window = glfwCreateWindow(1280, 720, "GLFW + OpenGL + Dear ImGui", nullptr, nullptr);
+    if (!window) return 1;
+
+    // Set window icon
+    // {
+    //     GLFWimage images[1];
+    //     images[0].pixels = stbi_load("assets/icon.png", &images[0].width, &images[0].height, 0, 4);
+    //     glfwSetWindowIcon(window, 1, images);
+    //     stbi_image_free(images[0].pixels);
+    // }
+
     glfwMakeContextCurrent(window);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
+    glfwSwapInterval(1);  // Enable vsync
+
+    if (gladLoadGL() == 0) {  // Initialize OpenGL loader
+        spdlog::error("Failed to initialize OpenGL loader!");
+        return 1;
     }
 
-    // Set the required callback functions
-    glfwSetKeyCallback(window, key_callback);
-
-    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
     {
-        std::cout << "Failed to initialize OpenGL context" << std::endl;
-        return -1;
+        int major, minor, rev;
+        glfwGetVersion(&major, &minor, &rev);
+        spdlog::info("GLFW {}.{}.{}, OpenGL {}", major, minor, rev, glGetString(GL_VERSION));
     }
 
-    // Define the viewport dimensions
-    glViewport(0, 0, WIDTH, HEIGHT);
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;      // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;    // Enable Multi-Viewport / Platform Windows
+    // io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
+    // io.ConfigViewportsNoAutoMerge = true;
+    // io.ConfigViewportsNoTaskBarIcon = true;
 
-    // Game loop
-    while (!glfwWindowShouldClose(window))
-    {
-        // Check if any events have been activated (key pressed, mouse moved etc.) and call corresponding response functions
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    // When viewports are enabled we tweak WindowRounding/WindowBg
+    // so platform windows can look identical to regular ones.
+    ImGuiStyle& style = ImGui::GetStyle();
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+        style.WindowRounding = 0.0f;
+        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+    }
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    // Load Fonts
+    io.Fonts->AddFontFromFileTTF("Roboto-Medium.ttf", 16.0f);
+
+    bool show_window = false;
+    auto clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
 
-        // Render
-        // Clear the colorbuffer
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        if (!io.WantCaptureKeyboard) {
+            // Handle keyboard input
+            if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, GLFW_TRUE);
+        }
+
+        if (!io.WantCaptureMouse) {
+            // Handle mouse input
+        }
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+            static char name[32] = "Peter";
+
+            ImGui::Begin("Hello, world!");
+
+            ImGui::Checkbox("Another Window", &show_window);
+
+            ImGui::InputText("Your name", name, IM_ARRAYSIZE(name));
+            ImGui::Text("Hello, %s!", name);
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+            ImGui::ColorEdit3("clear color", (float*)&clear_color);
+
+            if (ImGui::Button("Button")) counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+
+        ImGui::Render();
+
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Swap the screen buffers
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+            auto ctx = glfwGetCurrentContext();
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+            glfwMakeContextCurrent(ctx);
+        }
+
         glfwSwapBuffers(window);
     }
 
-    // Terminates GLFW, clearing any resources allocated by GLFW.
-    glfwTerminate();
-    return 0;
-}
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
-// Is called whenever a key is pressed/released via GLFW
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
-{
-    std::cout << key << std::endl;
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,14 @@
 add_rules("mode.debug", "mode.release")
 
+add_requires("fmt ^7.1.3", "glad ^0.1.34", "glfw ^3.3.4", "glm ^0.9.9", "spdlog ^1.8.5", "stb", "imgui-docking")
+
+target("test")
+    set_kind("binary")
+    set_languages("cxx17")
+    add_files("src/*.cpp")
+    add_packages("fmt", "glad", "glfw", "glm", "spdlog", "stb", "imgui-docking")
+
 package("imgui-docking")
-    set_homepage("https://github.com/ocornut/imgui/tree/docking")
-    set_description("Bloat-free Immediate Mode Graphical User interface for C++ with minimal dependencies")
     add_urls("https://github.com/ocornut/imgui/archive/ac35b4bba2c30ad3df88ce8b86aaa8877099fc96.tar.gz")
     add_versions("1.83", "dfdbbb9f7d7e0171e5388ad692de199811562d6f9aaf0d8bc39f93696c6e06f1")
 
@@ -11,37 +17,15 @@ package("imgui-docking")
     end
 
     on_install("macosx", "linux", "windows", "mingw", "android", "iphoneos", function (package)
-        local xmake_lua = [[
-            target("imgui")
+        io.writefile("xmake.lua", [[
+            add_requires("glad ^0.1.34", "glfw ^3.3.4")
+            target("imgui-docking")
                 set_kind("static")
                 add_includedirs(".")
                 add_files("*.cpp", "backends/imgui_impl_glfw.cpp", "backends/imgui_impl_opengl3.cpp")
                 add_headerfiles("imgui.h", "imconfig.h", "backends/imgui_impl_glfw.h", "backends/imgui_impl_opengl3.h")
-        ]]
-
-        io.writefile("xmake.lua", xmake_lua)
+                add_packages("glad", "glfw")
+        ]])
         import("package.tools.xmake").install(package)
     end)
-
-    on_test(function (package)
-        assert(package:check_cxxsnippets({test = [[
-            void test() {
-                IMGUI_CHECKVERSION();
-                ImGui::CreateContext();
-                ImGuiIO& io = ImGui::GetIO();
-                ImGui::NewFrame();
-                ImGui::Text("Hello, world!");
-                ImGui::ShowDemoWindow(NULL);
-                ImGui::Render();
-                ImGui::DestroyContext();
-            }
-        ]]}, {configs = {languages = "c++11"}, includes = {"imgui.h"}}))
-    end)
 package_end()
-
-add_requires("glad", "glfw", "imgui-docking")
-
-target("test")
-    set_kind("binary")
-    add_files("src/*.cpp")
-    add_packages("glad", "glfw", "imgui-docking")
